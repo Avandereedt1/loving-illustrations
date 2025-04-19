e05eeb5f0413452a8a2b903466f1cebcfc804f76
Avandereedt1 <Avandereedt1@owlmail.harford.edu>
2025-04-19 00:55:50 -0400

    java folder added and attempt at dark mode button unsuccessful

---
+++ java/main.java
@@ -0,0 +1,10 @@
+/*const introContainer = document.querySelector(".intro");
+const darkTrigger = document.querySelector("#dark-button");
+
+function darkToggle() {
+ 
+  introContainer.classList.toggle("dark");
+};
+
+darkTrigger.addEventListener("click", darkToggle);
+ 
